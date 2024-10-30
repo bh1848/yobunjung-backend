324ab66a290bcfd626858cec25c94639e658e6c1
Hyeok <bh1848@naver.com>
2024-10-30 16:41:51 +0900

    fix: yolo_model 수정

--- app/models/yolo_model.py
+++ app/models/yolo_model.py
@@ -6,7 +6,11 @@ warnings.filterwarnings("ignore", category=FutureWarning, module='torch.cuda.amp
 
 # 모델 로드 함수
 def load_model():
-    return torch.hub.load('ultralytics/yolov5', 'custom', path='exp6/weights/best.pt')
+    model_path = '/home/ubuntu/yobunjung/Flask/exp6/weights/best.pt'  # 절대 경로 사용
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path, force_reload=True, trust_repo=True)
+    model.to(device)
+    return model
 
 
 # 모델 인스턴스 생성
