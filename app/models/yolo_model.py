b2bca3e205dcaf0b9f408cf9d9ed3cd63c661824
Hyeok <bh1848@naver.com>
2024-10-30 16:45:09 +0900

    fix:yolo_model 수정

--- app/models/yolo_model.py
+++ app/models/yolo_model.py
@@ -3,15 +3,10 @@ import warnings
 
 warnings.filterwarnings("ignore", category=FutureWarning, module='torch.cuda.amp.autocast')
 
-
 # 모델 로드 함수
 def load_model():
-    model_path = '/home/ubuntu/yobunjung/Flask/exp6/weights/best.pt'  # 절대 경로 사용
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path, force_reload=True, trust_repo=True)
-    model.to(device)
-    return model
-
+    # Linux와 Windows에서 호환되도록 추가 옵션 사용
+    return torch.hub.load('ultralytics/yolov5', 'custom', path='exp6/weights/best.pt', force_reload=True, trust_repo=True)
 
 # 모델 인스턴스 생성
 model = load_model()
